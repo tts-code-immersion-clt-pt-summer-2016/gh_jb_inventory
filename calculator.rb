f7329ef5d65ef255f4452cc65c919bded9ad934a
Gerrick D. Hilliard <gerrickhilliard@yahoo.com>
2016-07-25 04:23:38 -0400

    Add calculator challenge

---
+++ calculator.rb
@@ -0,0 +1,44 @@
+def add(number, another_number)
+ return number + another_number 
+ number = @number
+end
+
+def subtract(numbers, another_numbers)
+ return numbers - another_numbers
+ numbers = @numbers
+end
+
+def sum(x)
+sum = 0
+x.each do |c|
+	sum+= c
+end
+sum
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
